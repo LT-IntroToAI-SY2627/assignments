a454ce33f14eeb0b1cb2e6cbe539f957f74b4d17
Jhonxel De Jesus <jdejesus20@cps.edu>
2026-09-15 16:33:46 +0000

    Add gh2

--- gh2/gh2.py
+++ gh2/gh2.py
@@ -1,7 +1,7 @@
 # gh2.py
 # GH-2: Functions & Conditionals
 # Introduction to AI | Lane Tech College Prep
-# Name: [Your Name Here]
+# Name: [Jhonxel De Jesus]
 
 # Instructions:
 # Replace each 'pass' with your solution.
@@ -19,7 +19,7 @@
 # multiply(2, 2)  ->  4
 
 def multiply(a, b):
-    pass
+    return a * b
 
 assert multiply(3, 4) == 12, "multiply test 1 failed"
 assert multiply(5, 0) == 0,  "multiply test 2 failed"
@@ -41,8 +41,15 @@ assert multiply(2, 2) == 4,  "multiply test 3 failed"
 # classify_temp(30)  ->  "cold"
 
 def classify_temp(temp):
-    pass
-
+   if temp >= 90:
+      return "hot"
+   elif temp >= 70:
+      return "warm"
+   elif temp >= 50:
+      return "cool"
+   else:
+      return "cold"
+   
 assert classify_temp(95) == "hot",  "classify_temp test 1 failed"
 assert classify_temp(75) == "warm", "classify_temp test 2 failed"
 assert classify_temp(55) == "cool", "classify_temp test 3 failed"
