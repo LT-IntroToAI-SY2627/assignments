72f5327e85ca963e8e1c4756bf7712e57f1ef187
Efren <ealbino2@cps.edu>
2026-09-18 17:14:40 +0000

    completed gh3 independent problems

--- gh3/gh3.py
+++ gh3/gh3.py
@@ -74,6 +74,17 @@ assert count_vowels("gym") == 0
 
 # Problem 6
 def fizzbuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+    return result
     
 
 assert fizzbuzz(1) == [1]
