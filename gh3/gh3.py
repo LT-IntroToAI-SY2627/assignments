9f45703a31e2fcc6f0836e2e5f5e86b855184da9
connor <crdarge@cps.edu>
2026-09-18 16:18:00 +0000

    finish problems 2 and 3

--- gh3/gh3.py
+++ gh3/gh3.py
@@ -18,20 +18,35 @@ assert sum_list([1, 2, 3]) == 6
 assert sum_list([]) == 0
 
 # Problem 2
-def count_above(numbers, threshold=10):
+def count_above(numbers, threshold=6):
     count = 0
     for num in numbers:
         if num > threshold:
             count += 1
     return count
 
-assert count_above([1, 2, 3, 4, 5], 3) == 2
-assert count_above([1, 2, 3,]) == 6
+assert count_above([1, 2, 3, 4, 5], 3) == 0
+assert count_above([1, 2, 3, 4, 5], 10) == 1
+
 
 # Problem 3
 def categorize_scores(scores):
-    pass
+    categories = []
+    for score in scores:
+        if score >= 90:
+            categories.append("A")
+        if 79.5 <= score < 89.4:
+            categories.append("B")
+        if 69.5 <= score < 79.4:
+            categories.append("C")
+        if 59.5 <= score < 69.4:
+            categories.append("D")
+        else:
+            categories.append("F")
+    return categories
 
+assert categorize_scores([95, 85, 75, 65, 55]) == ["A", "B", "C", "D", "F"]
+assert categorize_scores([100, 90, 80, 70, 60]) == ["A", "A", "B", "C", "D"]
 
 # =============================================================================
 # INDEPENDENT PROBLEMS
