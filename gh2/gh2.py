914e780baae600f4abc5476625edcae5c63498a0
Peter F <pjfavela@cps.edu>
2026-09-21 16:05:17 +0000

    Complete GH-2 functions and conditionals

--- gh2/gh2.py
+++ gh2/gh2.py
@@ -19,7 +19,8 @@
 # multiply(2, 2)  ->  4
 
 def multiply(a, b):
-    pass
+    my_product = a * b
+    return my_product
 
 assert multiply(3, 4) == 12, "multiply test 1 failed"
 assert multiply(5, 0) == 0,  "multiply test 2 failed"
@@ -41,7 +42,14 @@ assert multiply(2, 2) == 4,  "multiply test 3 failed"
 # classify_temp(30)  ->  "cold"
 
 def classify_temp(temp):
-    pass
+    if temp >= 90:
+        return "hot"
+    elif temp >= 70:
+        return "warm"
+    elif temp >= 50:
+        return "cool"
+    else:
+        return "cold"
 
 assert classify_temp(95) == "hot",  "classify_temp test 1 failed"
 assert classify_temp(75) == "warm", "classify_temp test 2 failed"
@@ -60,6 +68,6 @@ assert classify_temp(30) == "cold", "classify_temp test 4 failed"
 #   - At least 2 asserts that test it
 
 # Write your function here:
-
+def is_even(n):
 
 # Write your asserts here:
