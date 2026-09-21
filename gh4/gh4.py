9148aed30a6600a484bba2f4d0bbdc611df862c0
Simona Arsic <sarsic@cps.edu>
2026-09-21 16:23:05 +0000

    Commit gh4 lists assignment

--- gh4/gh4.py
+++ gh4/gh4.py
@@ -1,7 +1,7 @@
 # gh4.py
 # GH-4: Lists
 # Introduction to AI | Lane Tech College Prep
-# Name: [Your Name Here]
+# Name: Simona Arsic
 
 # =============================================================================
 # IN-CLASS PROBLEMS
@@ -10,18 +10,23 @@
 
 # Problem 1
 def get_first(lst):
-    pass
+    return lst[0]
 
-
-# Problem 2
+# Problem 2 
 def list_min(lst):
-    pass
-
+    min_num = lst[0]
+    for num in lst:
+        if num < min_num:
+            min_num = num
+    return min_num
 
 # Problem 3
 def sum_positive(lst):
-    pass
-
+    total = 0
+    for num in lst:
+        if num > 0:
+            total = total + num
+    return total
 
 # =============================================================================
 # INDEPENDENT PROBLEMS
@@ -31,7 +36,11 @@ def sum_positive(lst):
 
 # Problem 4
 def remove_duplicates(lst):
-    pass
+    noduo_lst = []
+    for item in lst:
+        if item not in noduo_lst:
+            noduo_lst.append(item)
+    return noduo_lst
 
 assert remove_duplicates([1, 2, 2, 3, 3, 3]) == [1, 2, 3]
 assert remove_duplicates([1, 1, 1]) == [1]
@@ -40,7 +49,7 @@ assert remove_duplicates([]) == []
 
 # Problem 5
 def every_other(lst):
-    pass
+    return lst[::2]
 
 assert every_other([1, 2, 3, 4, 5]) == [1, 3, 5]
 assert every_other([10, 20, 30]) == [10, 30]
@@ -49,7 +58,10 @@ assert every_other([]) == []
 
 # Problem 6
 def is_sorted(lst):
-    pass
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
 
 assert is_sorted([1, 2, 3, 4, 5]) == True
 assert is_sorted([1, 3, 2, 4, 5]) == False
