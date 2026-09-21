5d01f5694bc5d46c61124c8acfd32ca9f33b2d90
Efren <ealbino2@cps.edu>
2026-09-21 16:29:46 +0000

    Completed all gh4 list assignemts

--- gh4/gh4.py
+++ gh4/gh4.py
@@ -57,7 +57,10 @@ assert every_other([]) == []
 
 # Problem 6
 def is_sorted(lst):
-    pass
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
 
 assert is_sorted([1, 2, 3, 4, 5]) == True
 assert is_sorted([1, 3, 2, 4, 5]) == False
