32a2d74bed6b71c4442dbb604fe08c2232d48407
Leo Lawrence <ljlawrence1@cps.edu>
2026-09-21 16:16:14 +0000

    finished problem 5

--- gh4/gh4.py
+++ gh4/gh4.py
@@ -45,11 +45,11 @@ assert remove_duplicates([1, 1, 2, 3, 4, 4, 4]) == [1, 2, 3, 4]
 
 # Problem 5
 def every_other(lst):
-    pass
+    
 
 assert every_other([1, 2, 3, 4, 5]) == [1, 3, 5]
 assert every_other([10, 20, 30]) == [10, 30]
-assert every_other([]) == []
+assert every_other([100, 200, 300, 400]) == [100, 300]
 
 
 # Problem 6
