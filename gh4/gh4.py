c9ce0b441a22c8f3a37c69b12798ccc306075eaa
Robert Berg <rberg@cps.edu>
2026-09-24 16:11:00 +0000

    finished problem 1

--- gh4/gh4.py
+++ gh4/gh4.py
@@ -10,8 +10,10 @@
 
 # Problem 1
 def get_first(lst):
-    pass
+    return lst[0] if lst else None
 
+print(get_first([1, 2, 3]))  # Output: 1
+print(get_first([]))
 
 # Problem 2
 def list_min(lst):
