cd8e829ebed0b4a510a1def95628b60f5ede3482
Zoey B <zbrown13@cps.edu>
2026-09-24 16:23:14 +0000

    Finished class problems 1-3

--- gh4/gh4.py
+++ gh4/gh4.py
@@ -10,9 +10,10 @@
 
 # Problem 1
 def get_first(lst):
-    pass
+    return lst[0] if lst else None
 
-
+print(get_first([1, 2, 3]))  # Output: 1
+print(get_first([]))        # Output: None
 # Problem 2
 def list_min(lst):
     pass
